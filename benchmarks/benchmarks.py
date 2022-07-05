ebb0e6da2ede3f64645f4f88213f6debd13166cc
Holly Wingren <wingren2@illinois.edu>
2022-07-05 11:47:26 -0500

    First query imported

--- benchmarks/benchmarks.py
+++ benchmarks/benchmarks.py
@@ -12,23 +12,33 @@ from coffea.nanoevents import schemas
 
 class TimeSuite:
     fileset = {'SingleMu' : ["root://eospublic.cern.ch//eos/root-eos/benchmark/Run2012B_SingleMu.root"]}
-    def time_Q1(self):
-        def process(self, events):
-            return (
-                hist.Hist.new.Reg(100, 0, 200, name="met", label="$E_{T}^{miss}$ [GeV]")
-                .Double()
-                .fill(events.MET.pt)
-            )
-        def postprocess(self, accumulator):
-            return accumulator
-    executor = processor.IterativeExecutor()
-    run = processor.Runner(executor=executor,
+    def TimeQ1(self):
+        class Q1Processor(processor.ProcessorABC):
+            def process(self, events):
+                return (
+                    hist.Hist.new.Reg(100, 0, 200, name="met", label="$E_{T}^{miss}$ [GeV]")
+                    .Double()
+                    .fill(events.MET.pt)
+                )
+            def postprocess(self, accumulator):
+                return accumulator
+        if os.environ.get("LABEXTENTION_FACTORY_MODULE") == "coffea_casa":
+            from dask.distributed import Client
+            client = Client("tls://localhost:8786")
+            executor = processor.DaskExecutor(client=client)
+        else:
+            executor = processor.IterativeExecutor()
+        run = processor.Runner(executor=executor,
                        schema=schemas.NanoAODSchema,
                        savemetrics=True,
                        chunksize=2**19,
                       )
-
-
+        tic = time.monotonic()
+        output, metrics = run(fileset, "Events", processor_instance=Q1Processor())
+        toc = time.monotonic()
+        metrics["walltime"] = toc - tic
+        metrics
+    
     def setup(self):
         self.d = {}
         for x in range(500):
