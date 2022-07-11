ea73fde5114a5534d5fca9033aa214e3ec508788
HollyWingren <wingren2@illinois.edu>
2022-07-11 14:47:39 +0000

    Attempt at ncores

--- benchmarks/Q1_Parameters.py
+++ benchmarks/Q1_Parameters.py
@@ -10,7 +10,7 @@ from coffea.nanoevents import schemas
 fileset = {'SingleMu' : ["root://eospublic.cern.ch//eos/root-eos/benchmark/Run2012B_SingleMu.root"]}
 class TimeSuite:
     timeout = 1200.00
-    def TimeQ1(self, n):
+    def TimeQ1(self, n, ncores):
         class Q1Processor(processor.ProcessorABC):
             def process(self, events):
                 return (
@@ -21,15 +21,29 @@ class TimeSuite:
             def postprocess(self, accumulator):
                 return accumulator
         if os.environ.get("LABEXTENTION_FACTORY_MODULE") == "coffea_casa":
-            from dask.distributed import Client
-            client = Client("tls://localhost:8786")
-            executor = processor.DaskExecutor(client=client)
+            #from dask.distributed import Client
+            #client = Client("tls://localhost:8786")
+            executor = processor.FuturesExecutor(workers=ncores, status=False)
         else:
-            executor = processor.IterativeExecutor()
+            #executor = processor.IterativeExecutor()
+            executor = processor.FuturesExecutor(workers=ncores, status=False)
         run = processor.Runner(executor=executor,
                        schema=schemas.NanoAODSchema,
                        savemetrics=True,
-                       chunksize=n,
+                       chunksize=n
                       )
         run(fileset, "Events", processor_instance=Q1Processor())
-    TimeQ1.params = [2 ** 17, 2 ** 18, 2 ** 19]
+    TimeQ1.params = ([2 ** 17, 2 ** 18, 2 ** 19], [1, 2])
+    
+    
+    
+    
+    
+    
+    
+    
+#    def time_ranges(n, func_name):
+ #   f = {'range': range, 'arange': numpy.arange}[func_name]
+  #  for i in f(n):
+   #     pass
+   # time_ranges.params = ([10, 1000], [2, 4])
